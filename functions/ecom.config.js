bbb711b4da0586598f65624844986e3fffc7a258
Leonardo Matos <leomp120894@gmail.com>
2021-01-29 20:40:50 -0300

    chore(ecom-config): basic app setup for calculate shipping module only

--- functions/ecom.config.js
+++ functions/ecom.config.js
@@ -7,8 +7,8 @@
 
 const app = {
   app_id: 115229,
-  title: 'My Awesome E-Com Plus App',
-  slug: 'my-awesome-app',
+  title: 'Jadlog',
+  slug: 'jadlog',
   type: 'external',
   state: 'active',
   authentication: true,
@@ -22,7 +22,7 @@ const app = {
      * Triggered to calculate shipping options, must return values and deadlines.
      * Start editing `routes/ecom/modules/calculate-shipping.js`
      */
-    // calculate_shipping:   { enabled: true },
+    calculate_shipping:   { enabled: true },
 
     /**
      * Triggered to validate and apply discount value, must return discount and conditions.
@@ -41,100 +41,6 @@ const app = {
      * Start editing `routes/ecom/modules/create-transaction.js`
      */
     // create_transaction:   { enabled: true },
-  },
-
-  /**
-   * Uncomment only the resources/methods your app may need to consume through Store API.
-   */
-  auth_scope: {
-    'stores/me': [
-      'GET'            // Read store info
-    ],
-    procedures: [
-      'POST'           // Create procedures to receive webhooks
-    ],
-    products: [
-      // 'GET',           // Read products with public and private fields
-      // 'POST',          // Create products
-      // 'PATCH',         // Edit products
-      // 'PUT',           // Overwrite products
-      // 'DELETE',        // Delete products
-    ],
-    brands: [
-      // 'GET',           // List/read brands with public and private fields
-      // 'POST',          // Create brands
-      // 'PATCH',         // Edit brands
-      // 'PUT',           // Overwrite brands
-      // 'DELETE',        // Delete brands
-    ],
-    categories: [
-      // 'GET',           // List/read categories with public and private fields
-      // 'POST',          // Create categories
-      // 'PATCH',         // Edit categories
-      // 'PUT',           // Overwrite categories
-      // 'DELETE',        // Delete categories
-    ],
-    customers: [
-      // 'GET',           // List/read customers
-      // 'POST',          // Create customers
-      // 'PATCH',         // Edit customers
-      // 'PUT',           // Overwrite customers
-      // 'DELETE',        // Delete customers
-    ],
-    orders: [
-      // 'GET',           // List/read orders with public and private fields
-      // 'POST',          // Create orders
-      // 'PATCH',         // Edit orders
-      // 'PUT',           // Overwrite orders
-      // 'DELETE',        // Delete orders
-    ],
-    carts: [
-      // 'GET',           // List all carts (no auth needed to read specific cart only)
-      // 'POST',          // Create carts
-      // 'PATCH',         // Edit carts
-      // 'PUT',           // Overwrite carts
-      // 'DELETE',        // Delete carts
-    ],
-
-    /**
-     * Prefer using 'fulfillments' and 'payment_history' subresources to manipulate update order status.
-     */
-    'orders/fulfillments': [
-      // 'GET',           // List/read order fulfillment and tracking events
-      // 'POST',          // Create fulfillment event with new status
-      // 'DELETE',        // Delete fulfillment event
-    ],
-    'orders/payments_history': [
-      // 'GET',           // List/read order payments history events
-      // 'POST',          // Create payments history entry with new status
-      // 'DELETE',        // Delete payments history entry
-    ],
-
-    /**
-     * Set above 'quantity' and 'price' subresources if you don't need access for full product document.
-     * Stock and price management only.
-     */
-    'products/quantity': [
-      // 'GET',           // Read product available quantity
-      // 'PUT',           // Set product stock quantity
-    ],
-    'products/variations/quantity': [
-      // 'GET',           // Read variaton available quantity
-      // 'PUT',           // Set variation stock quantity
-    ],
-    'products/price': [
-      // 'GET',           // Read product current sale price
-      // 'PUT',           // Set product sale price
-    ],
-    'products/variations/price': [
-      // 'GET',           // Read variation current sale price
-      // 'PUT',           // Set variation sale price
-    ],
-
-    /**
-     * You can also set any other valid resource/subresource combination.
-     * Ref.: https://developers.e-com.plus/docs/api/#/store/
-     */
   }
 }
 
